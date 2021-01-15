b52a38d1038f7a72eefca03d4f95577a37b2331b
gabrielsaimo <gabrielsaimo68@gmail.com>
2021-01-15 08:39:40 -0300

    g

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 
-<?php include("cb.php");
+<? include("cb.php");
 
     $grupo = selectAllPessoa();
-    
 ?>
 
 <html lang="pt-br">
@@ -11,9 +10,33 @@
     <meta charset="UTF-8">
     <title>CRUD com PHP e MYSQL - INSERT</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
+  
+<script>
+
+$(document).ready(function(){
+    $(".delete").click(function(){debugger;
+        var id=$(this).attr("data-id");
+        var acao=$(this).attr("acao")
+            $.ajax({
+            url: 'cb.php',
+            type: 'POST',
+            data:{ id: $("#id_d").val() ,acao:$("#acao_a").val(),
+            },
+            success: function(r) {
+                location.reload();
+            },
+            error: function(r){
+                alert('deu erro');
+            }
+        });
+        });
+    });
+    
+</script>
 <div class="container">
     <body>
      <div class="posicionarCabecalho">
@@ -31,46 +54,43 @@
               </tr>
           </thead>
           <tbody>
-             <?php 
+             <?
                 foreach($grupo as $pessoa) { ?>
                    <tr>
-                      <th><?=$pessoa["nome"]?></th>
-                      <th><?=$pessoa["sexo"]?></th>
-                      <th><?=$pessoa["criadoem"]?></th>
-                      <th><?=$pessoa["atualizadoem"]?></th>
-                      <th>
-                          <form name="alterar" action="inserir.php" method="post">
-                              <input type="hidden" name="id" value="<?=$pessoa["id"]?>">
-                              <input class="btn_edit" type="submit" name="editar" value="Editar">
-                          </form>
-                      </th>
-                      <th>
-                          <form name="excluir" action="cb.php" method="post">
-                              <input type="hidden" name="id" value="<?=$pessoa["id"]?>">
-                              <input type="hidden" name="acao" value="excluir">
-                              <input class="btn_excloi" type="submit" name="excluit" value="Excluir">
-                          </form>
-                      </th>
-                  </tr>   
-                <?php }
-              ?>
+                      <td><?=$pessoa["nome"]?></td>
+                      <td><?=$pessoa["sexo"]?></td>
+                      <td><?=$pessoa["criadoem"]?></td>
+                      <td><?=$pessoa["atualizadoem"]?></td>
+                      <td>
+                         <!---  name="alterar" action="inserir.php" method="post" data-id="<?php echo $pessoa["id"];?>" name="excluir" action="cb.php" method="post"            href="inserir.php?id=<?=$pessoa["id"]?>&acao=excluir" ---->
+                         <a class="color-black btn fundo-amarelo" href="inserir.php?id=<?=$pessoa["id"]?>">
+                             <b>Editar</b>
+                         </a>
+                      </td>
+                        <td>
+                            <a class="btn fundo-vermelho delete" data-id="<?php echo $pessoa["id"];?>" acao="excluir"  >
+                                <b>Excluir</b>
+                            </a>
+                        </td>
+                  </tr>
+                <?}?>
           </tbody>
      </table>
       <div class="text-center">
-           <button type="button" class="btn btn-primary"><a href="inserir.php">Adicionar</a></button>
+            <button type="button" class="btn btn-primary">
+               <a href="inserir.php">Adicionar</a>
+            </button>
       </div>
       <div style="display: flex; justify-content: center;">
       <img src="img/pato.gif" style="width: 130px;" />
       </div>
-      
-       <?php
+       <?
             // Função para formatar a data
             function formatoData($data) {
                 $array = explode("-", $data);
                 $novaData = $array[2]."/".$array[1]."/".$array[0];
                 return $novaData;
             }
-
         ?>
     </body>
 </div>
--- inserir.php
+++ inserir.php
@@ -1,31 +1,30 @@
-<?php
+<?
 
 include("cb.php");
-
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <meta charset="UTF-8">
 <div class="container">
-    <form  action="cb.php" method="post">
+    <form  action="cb.php" method="$_GET" >
         <table>
             <tbody>
-            <?if($_POST["id"] != null ){?>
-                <?$pessoa = selectIdPessoa($_POST["id"]);?>
+            <?if (isset($_GET["id"]) != null ){?>
+                <?$pessoa = selectIdPessoa($_GET["id"]);?>
             <?}?>
                 <tr>
                     <td>Nome: </td>
-                    <?if($_POST["id"] != null ){?>
-                        <td><input type="text" name="nome" value="<?=$pessoa["nome"]?>" size="20"></td>
+                    <?if(isset($_GET["id"]) != null ){?>
+                        <td><input id="nome" type="text" name="nome" value="<?=$pessoa["nome"]?>" size="20"></td>
                     <?}else{?>
                         <td><input type="text" name="nome" value=""></td>
                     <?}?>
                 </tr>
                 <tr>
                     <td>Sexo: </td>
-                    <?if($_POST["id"] != null ){?>
-                        <td><select name="sexo" value="<?=$pessoa["sexo"]?>">
+                    <?if(isset($_GET["id"]) != null ){?>
+                        <td><select name="sexo" value="">
                             <option value="Maxoxo">Maxoxo</option>
                             <option value="Femiamia">Femiamia</option>
                         </select>
@@ -39,7 +38,7 @@ include("cb.php");
                     <?}?>
                 </tr>
                 <tr>
-                    <?if($_POST["id"] != null ){?>
+                    <?if(isset($_GET["id"]) != null ){?>
                         <td><input type="hidden" name="acao" value="alterar">
                     <?}else{?>
                         <td><input type="hidden" name="acao" value="inserir">
